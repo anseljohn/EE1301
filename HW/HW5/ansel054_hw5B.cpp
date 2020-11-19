be25db6d9294a0be2e049a14c6101328a6cac9ab
John Anselmo <jjohnanselmo@gmail.com>
2020-11-18 20:38:04 -0600

    Absolutely painful

---
+++ HW/HW5/ansel054_hw5B.cpp
@@ -0,0 +1,41 @@
+// EE 1301 
+// HW 5B
+// John Anselmo
+// ansel054
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string.h>
+#include <fstream>
+
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        cout << "ERROR: Incorrect number of arguments.\nProgram requires 3\n";
+        return 1;
+    } else {
+        srand(time(NULL));
+
+        ofstream outputFile;
+        outputFile.open("./randArray.txt");
+
+        const int rows = stoi((string) argv[1]);
+        const int columns = stoi((string) argv[2]);
+        const int max = stoi((string) argv[3]);
+
+        for (int rowNum = 0; rowNum < rows; rowNum++) {
+            for (int colNum = 0; colNum < columns; colNum++) {
+                outputFile << (rand() % ((2 * max) + 1)) - max;
+                if (colNum != columns - 1) { 
+                    outputFile << " ";
+                }
+            }
+            outputFile << endl;
+        }
+    }
+
+    return 0;
+}
