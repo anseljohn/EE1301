4bb2f3c9d927fcd8ca69d5e570526de1d12dff6e
anseljohn <jjohnanselmo@gmail.com>
2020-11-30 00:50:47 -0600

    Updated gitignore in effect

--- Labs/IoT/Lab4/target/1.5.2/photon/obj/src/user_module.o.d
+++
@@ -1,4 +0,0 @@
-/Users/owner/Documents/School/ee1301Main/EE1301/Labs/IoT/Lab4/target/1.5.2/photon//obj/src/user_module.o: \
- src/user_module.c ../../../dynalib/inc/dynalib.h
-
-../../../dynalib/inc/dynalib.h:
