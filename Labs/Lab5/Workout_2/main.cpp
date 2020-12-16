3d248e29635bee2e9533b2031b45249dd59de06a
anseljohn <jjohnanselmo@gmail.com>
2020-12-16 00:33:09 -0500

    Giving up on myself

--- Labs/Lab5/Workout_2/main.cpp
+++ Labs/Lab5/Workout_2/main.cpp
@@ -10,46 +10,34 @@ double *randVec();
 
 int main () {
     srand(time(NULL));
-    for (int i = 0; i < 10; i++) {
-        double *momentums;
-        double velocity[3] = {0};
-        double mass = rand() % 100;
-
-        cout << "V: {";
-        for (int i = 0; i < 2; i++) {
-            velocity[i] = rand() % 50;
-            cout << velocity[i] << " ";
-        }
-        velocity[2] = rand() % 50;
-        cout << velocity[2] << "}\nM: ";
-
-        cout << mass << endl;
-
-        momentums = momentum(velocity, mass);
-    
+    // for (int i = 0; i < 10; i++) {
+        double *momentums = momentum(randVec(), (rand() % 9) + 1);
         for ( int i = 0; i < 3; i++ ) {
             cout << *(momentums + i) << endl;
         }
+        delete[] momentums;
         cout << endl << endl;
-    }
+    // }
  
    return 0;
 }
 
-double *momentum(double velocity[3], double mass) {
+double *momentum(double velocity[], double mass) {
     double momentum[3];
     for (int i = 0; i < 3; i++) {
-        momentum[i] = velocity[i] * mass;
+        double vel = velocity[i];
+        momentum[i] = vel * mass;
+        cout << momentum[i] << endl;
     }
 
     return momentum;
 }
 
 double *randVec() {
-    double velocities[3];
+    double *velocities[3];
     for (int i = 0; i < 3; i++) {
         velocities[i] = (rand() % 200) - 100;
     }
-    
+
     return velocities;
 }
